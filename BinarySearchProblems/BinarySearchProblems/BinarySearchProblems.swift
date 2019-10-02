c9e9c2186603c69f10904f56080e085607e72dd9
phoenixmcknight <phoenixmcknight@gmail.com>
2019-10-02 17:42:08 -0400

    g

--- BinarySearchProblems/BinarySearchProblems/BinarySearchProblems.swift
+++ BinarySearchProblems/BinarySearchProblems/BinarySearchProblems.swift
@@ -2,17 +2,52 @@ import Foundation
 
 //
 
+func newBinarySearch<T:Comparable> (arr:[T], target: T, range: Range<Int>? = nil) -> Bool {
+    //
+    let searchRange:Range<Int>
+    
+    if let range = range {
+        searchRange = range
+        
+    } else {
+        searchRange = (0..<arr.endIndex)
+    
+    }
+    if searchRange.lowerBound >= searchRange.upperBound {
+        return false
+    }
+    //
+    let middleIndex = (searchRange.lowerBound + searchRange.upperBound) / 2
+    if target == arr[middleIndex] {
+        return true
+    } else if target < arr[middleIndex] {
+        return newBinarySearch(arr: arr, target: target, range: searchRange.lowerBound..<middleIndex)
+    } else {
+        return newBinarySearch(arr: arr, target: target, range: (middleIndex + 1)..<searchRange.upperBound )
+    }
+}
 // 1. Find if an element is contained within a sorted array in O(log(n)) time.  Do not use recursion.
 
 func binarySearch<T: Comparable>(arr: [T], target: T) -> Bool {
+    var lowerBound = 0
+    var upperBound = arr.count
+    while lowerBound < upperBound {
+        let midIndex = lowerBound + (upperBound - lowerBound) / 2
+        if arr[midIndex] == target {
+            return true
+        } else if arr[midIndex] < target {
+            lowerBound = midIndex + 1
+        } else {
+            upperBound = midIndex
+        }
+    }
     return false
 }
 
 // 2. Find if an element is contained within a sorted array in O(log(n)) time.  Use recursion.
 
 func recursiveBinarySearch<T: Comparable>(arr: [T], target: T) -> Bool {
-    return false
-}
+newBinarySearch(arr: arr, target: target)}
 
 // https://www.interviewbit.com/problems/matrix-search/
 
@@ -22,8 +57,25 @@ func recursiveBinarySearch<T: Comparable>(arr: [T], target: T) -> Bool {
 // The first integer of each row is greater than or equal to the last integer of the previous row.
 
 func contains(value: Int, in arr: [[Int]]) -> Bool {
-    return false
-}
+var minIndex = 0
+    var maxIndex = arr.count - 1
+    
+    while minIndex < maxIndex {
+        let middleIndex = (minIndex + maxIndex)/2
+        if arr[middleIndex][0] == value {
+            return true
+        } else if value < arr[middleIndex][0] {
+           maxIndex = middleIndex - 1
+        } else if value > arr[middleIndex][0] {
+            minIndex = middleIndex + 1
+                
+            }
+        }
+    let targetArr = arr[minIndex]
+    return newBinarySearch(arr: targetArr, target: value)
+    
+    }
+
 
 
 
